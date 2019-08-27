84cb019fae28b02d5965dc3858615b3573f3d8e7
Tom Konidas <tomkonidas@protonmail.ch>
2019-08-27 04:29:24 -0400

    add roboto font

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ export default function (Vue, { router, head, isClient }) {
 
   head.link.push({
     rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css?family=Robot:400,700'
+    href: 'https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap'
   })
 }
 
