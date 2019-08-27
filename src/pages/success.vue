4298d8014f9c3f90d26fd4cfa6489e3d84665d0a
Tom Konidas <tomkonidas@protonmail.ch>
2019-08-27 13:05:20 -0400

    update success page message

--- src/pages/success.vue
+++ src/pages/success.vue
@@ -1,7 +1,10 @@
 <template>
   <Layout>
     <div class="container-inner mx-auto py-16">
-      <h2 class="text-4xl font-bold mb-16">Email sent successfully!</h2>
+      <h2 class="text-4xl font-bold mb-16">
+        <span class="text-6xl text-indigo-700">Hooray!</span>
+        <br />Email sent successfully!
+      </h2>
       <g-image src="../../static/mention.svg" />
     </div>
   </Layout>
