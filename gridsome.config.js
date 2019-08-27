0b44d16514fd102b1aad1657b0d240741ba01c02
Tom Konidas <tomkonidas@protonmail.ch>
2019-08-27 04:00:39 -0400

    redesign

---
+++ gridsome.config.js
@@ -0,0 +1,83 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+const tailwind = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+const postcssPlugins = [
+  tailwind(),
+]
+
+if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
+
+module.exports = {
+  siteName: 'Tom Konidas',
+  siteDescription: 'A place where I can showcase my work and let my knowledge flow.',
+  siteUrl: 'https://tomkonidas.com',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'blog/**/*.md',
+        typeName: 'Post',
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            route: 'tag/:id',
+            create: true
+          }
+        },
+        remark: {
+          plugins: [
+            ['gridsome-plugin-remark-shiki', { theme: 'Material-Theme-Palenight', skipInline: true }]
+          ]
+        }
+      }
+    },
+    {
+      use: 'gridsome-plugin-rss',
+      options: {
+        contentTypeName: 'Post',
+        feedOptions: {
+          title: 'Tom Konidas Feed',
+          feed_url: 'https://tomkonidas.com/rss.xml',
+          site_url: 'https://tomkonidas.com/'
+        },
+        feedItemOptions: node => ({
+          title: node.title,
+          description: node.summary,
+          url: 'https://tomkonidas.com' + node.path,
+          author: 'Tom Konidas',
+          date: node.date
+        }),
+        output: {
+          dir: './static',
+          name: 'rss.xml'
+        }
+      }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000, // default
+      }
+    },
+  ],
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+    }
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: postcssPlugins,
+      },
+    },
+  },
+}
---
+++ src/main.js
@@ -0,0 +1,39 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
+import DefaultLayout from '~/layouts/Default.vue'
+import VueScrollTo from 'vue-scrollto'
+import VueFuse from 'vue-fuse'
+
+export default function (Vue, { router, head, isClient }) {
+  // Set default layout as a global component
+  Vue.component('Layout', DefaultLayout)
+
+  Vue.use(VueScrollTo, {
+    duration: 500,
+    easing: "ease",
+  })
+
+  Vue.use(VueFuse)
+
+  head.meta.push({
+    name: 'keywords',
+    content: 'Web Development, Software, Services'
+  })
+
+  head.meta.push({
+    name: 'description',
+    content: 'A place where I can showcase my work and let my knowledge flow.'
+  })
+
+  head.meta.push({
+    name: 'author',
+    content: 'Tom Konidas'
+  })
+
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Robot:400,700'
+  })
+}
+
